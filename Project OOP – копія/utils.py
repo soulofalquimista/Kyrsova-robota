b7ea6a2961f3ee273d0af959acc72001c30c42a2
Назар Кулішов <158081111+soulofalquimista@users.noreply.github.com>
2024-11-29 16:09:10 +0200

    Update utils.py

--- Project OOP – копія/utils.py
+++ Project OOP – копія/utils.py
@@ -1,12 +1,10 @@
 import tkinter as tk
 
 def clear_entries(*entries):
-    """Очищення полів вводу"""
     for entry in entries:
         entry.delete(0, tk.END)
 
 def update_listbox(listbox, items):
-    """Оновлення списків у Listbox"""
     listbox.delete(0, tk.END)
     for item in items:
         listbox.insert(tk.END, item)
